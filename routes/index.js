658056bbbd1c7039e0b211c938a89e153b08748a
maulana <maulana.robert.mr@gmail.com>
2017-03-24 01:32:36 +0700

    1st test & debug

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,37 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express');
+const router = express.Router();
+const db = require("../models");
+const dayHelper = require("../helper/day");
+const monthHelper = require("../helper/month");
+
 
 router.get('/', function (req, res) {
-  res.render('index')
+  db.Product
+    .findAndCountAll()
+    .then((data) => {
+      db.Order
+        .findAndCountAll()
+        .then((countOrder) => {
+          let date = new Date();
+          let day = date.getDate();
+          let month = date.getMonth()+1;
+          db.Order
+            .findAll()
+            .then((dataOrder) => {
+              let countPerDay = 0;
+              let countPerMonth = 0;
+              dataOrder.forEach((countDay) => {
+                if (day == dayHelper(countDay.createdAt)) {
+                  countPerDay++;
+                }
+                if (month == monthHelper(countDay.createdAt)) {
+                  countPerMonth++;
+                }
+              })
+              res.render('index', {countProduct: data.count, countOrder: countOrder.count, countday: countPerDay, countMonth: countPerMonth})
+            })
+        })
+    })
 })
 
 module.exports = router
